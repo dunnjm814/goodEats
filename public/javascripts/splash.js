2d443cee4ec334fd86988d314261f4f79eacca1a
Jesse <jbenlind@gmail.com>
2021-01-05 17:41:09 -0600

    splash page buttons working

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -4,9 +4,40 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const logInForm = document.getElementById("loginForm");
     const demoUserForm = document.getElementById("demoUserForm")
 
-    formHolder.remove("signupForm");
-    formHolder.remove("loginForm");
-    formHolder.remove("demoUserForm");
+    const signUpButton = document.getElementById("signUpButton")
+    const logInButton = document.getElementById("logInButton")
+    const demoUserButton = document.getElementById("demoButton")
 
+    signUpButton.addEventListener("click", (event) => {
 
+        if(signUpForm.classList.contains("hidden")) {
+            signUpForm.classList.remove("hidden")
+            logInForm.classList.add("hidden")
+            demoUserForm.classList.add("hidden")
+        } else {
+            signUpForm.classList.add("hidden")
+        }
+    })
+
+    logInButton.addEventListener("click", (event) => {
+
+        if(logInForm.classList.contains("hidden")) {
+            logInForm.classList.remove("hidden")
+            demoUserForm.classList.add("hidden")
+            signUpForm.classList.add("hidden")
+        } else {
+            logInForm.classList.add("hidden")
+        }
+    })
+
+    demoUserButton.addEventListener("click", (event) => {
+
+        if(demoUserForm.classList.contains("hidden")) {
+            demoUserForm.classList.remove("hidden")
+            logInForm.classList.add("hidden")
+            signUpForm.classList.add("hidden")
+        } else {
+            demoUserForm.classList.add("hidden")
+        }
+    })
 })
