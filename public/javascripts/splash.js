8a9cae7830635e2d6959a8ee5a77bb5ae9b369ed
Jesse <jbenlind@gmail.com>
2021-01-05 16:36:35 -0600

    splash form editing

---
+++ public/javascripts/splash.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const formHolder = document.getElementById("formHolder");
+    const signUpForm = document.getElementById("signupForm");
+    const logInForm = document.getElementById("loginForm");
+    const demoUserForm = document.getElementById("demoUserForm")
+
+    formHolder.remove("signupForm");
+    formHolder.remove("loginForm");
+    formHolder.remove("demoUserForm");
+
+
+})
