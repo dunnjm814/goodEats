3282324c5f976a36362b9d21f6a10b411e6a5bc6
Taten <knight.taten@gmail.com>
2021-01-06 12:14:20 -0600

    Finished home route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const recipesRouter = require('./routes/recipes')
+const { restoreUser } = require('./sign-in-auth.js');
 
 const app = express();
 
@@ -25,13 +26,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 const store = new SequelizeStore({ db: sequelize });
 
 app.use(
-  session({
-    secret: 'superSecret',
-    store,
-    saveUninitialized: false,
-    resave: false,
-  })
+    session({
+        secret: 'superSecret',
+        store,
+        saveUninitialized: false,
+        resave: false,
+    })
 );
+
+app.use(restoreUser)
 
 // create Session table if it doesn't already exist
 store.sync();
@@ -41,19 +44,19 @@ app.use('/users', usersRouter);
 app.use('/recipes', recipesRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { render } = require('../app.js');
+const router = express.Router();
+const { restoreUser } = require('../sign-in-auth.js');
+const db = require("../db/models");
+const { csrfProtection, asyncHandler } = require("./utils");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const user = {userName: null, email: null, password: null , confirmedPassword: null}
-  res.render("splash", { user });
+router.get('/', csrfProtection, (req, res, next) => {
+    //const user = db.User.build();
+    console.log(req)
+    if (res.locals.authenticated) {
+        return res.redirect('/dashboard')
+    }
 
+    const user = { userName: null, email: null, password: null, confirmedPassword: null }
+    res.render("splash", {
+        title: "Sign-up",
+        user,
+        token: req.csrfToken(),
+    })
 });
+
+router.get('/dashboard', csrfProtection, asyncHandler(async(req, res, next) => {
+    const userId = res.locals.user.id;
+    const user = await db.User.findByPk(userId);
+    res.render('dashboard', { user });
+}));
 
 module.exports = router;
