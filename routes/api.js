f3e2828088381781e329e772acf1448628d06fbd
Jason Dunn <dunnjm814@gmail.com>
2021-01-08 11:50:52 -0700

    resolved issues from code review

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../db/models");
+const {Review} = require("../db/models");
 const { asyncHandler } = require("./utils");
 
-const createReview = db.Review
+
 router.post(
   "/recipes/:id(\\d+)/review",
   asyncHandler(async (req, res) => {
@@ -14,7 +14,7 @@ router.post(
 
     const recipeId = req.params.id
     const { revContent, rating } = req.body
-    const review = await createReview.create({ userId, recipeId, revContent, rating: rating[0] })
+    const review = await Review.create({ userId, recipeId, revContent, rating: rating[0] })
     res.json({ review, userName });
 
   })
