d27e7a020f2298529db9034b3c9fd8ec027ee207
Jason Dunn <dunnjm814@gmail.com>
2021-01-08 13:38:30 -0700

    commiting jbranch before pull main

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../db/models");
+const {Review, Recipe} = require("../db/models");
 const { asyncHandler } = require("./utils");
 
-const createReview = db.Review
 router.post(
   "/recipes/:id(\\d+)/review",
   asyncHandler(async (req, res) => {
@@ -14,15 +13,15 @@ router.post(
 
     const recipeId = req.params.id
     const { revContent, rating } = req.body
-    const review = await createReview.create({ userId, recipeId, revContent, rating: rating[0] })
-    const recipe = await db.Recipe.findByPk(recipeId)
+    const review = await Review.create({ userId, recipeId, revContent, rating: rating[0] })
+    const recipe = await Recipe.findByPk(recipeId)
     const avgRating = recipe.avgRating
     let numReview = recipe.numReview
     let tempSum = avgRating * numReview
     numReview++
     let finalSum = (tempSum + rating[0])
     let finalAvg = Math.ceil(finalSum / numReview)
-    await db.Recipe.update({ avgRating: finalAvg, numReviews: numReview }, {
+    await recipe.update({ avgRating: finalAvg, numReviews: numReview }, {
       where: {
         id:recipeId
       }
