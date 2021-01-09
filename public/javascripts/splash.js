a07a8755dbd3b1c44cd0714335af87c1dfd5b6fe
Jesse <jbenlind@gmail.com>
2021-01-09 01:26:58 -0600

    splash page completed

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -1,80 +1,92 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+  const formHolder = document.getElementById('innerForm');
+  const signUpForm = document.getElementById('signupForm');
+  const logInForm = document.getElementById('loginForm');
+  const demoUserForm = document.getElementById('demoUserForm');
+  const header = document.getElementById('header-1');
 
-const formHolder = document.getElementById('innerForm');
-const signUpForm = document.getElementById('signupForm');
-const logInForm = document.getElementById('loginForm');
-const demoUserForm = document.getElementById('demoUserForm')
+  const logInButton = document.getElementById('logInButton');
+  const signUpButton = document.getElementById('signUpButton');
+  const demoUserButton = document.getElementById('demoButton');
 
-const loginErrorAlert = document.querySelector('.loginErrors');
-const signUpErrorAlert = document.querySelector('.signUpErrors');
-const logInButton = document.getElementById('logInButton');
-const signUpButton = document.getElementById('signUpButton');
-const demoUserButton = document.getElementById('demoButton');
-const header = document.getElementById('header-1')
+  const loginErrorAlert = document.querySelector('.loginErrors');
+  const signUpErrorAlert = document.querySelector('.signUpErrors');
 
-const fadeIn = [logInButton, signUpButton, demoUserButton, header]
+  const fadeIn = [logInButton, signUpButton, demoUserButton, header];
 
-if(!loginErrorAlert && !signUpErrorAlert) {
-    fadeIn.forEach(element => element.classList.add('fade-in'))
+  if (!loginErrorAlert && !signUpErrorAlert) {
+    fadeIn.forEach((element) => element.classList.add('fade-in'));
+  } else if (loginErrorAlert) {
+    logInButton.classList.add('hidden');
+    formHolder.classList.remove('hidden');
+    logInForm.classList.remove('hidden');
+  } else {
+    signUpForm.classList.remove('hidden');
+    formHolder.classList.remove('hidden');
+    signUpButton.classList.add('hidden');
+  }
 
-    } else if(loginErrorAlert) {
-        logInButton.classList.add('hidden')
-        logInForm.classList.remove('hidden')
-        formHolder.classList.remove('hidden')
+  logInButton.addEventListener('click', (event) => {
+    logInButton.classList.add('hidden');
+    signUpButton.classList.remove('hidden');
+    demoUserButton.classList.remove('hidden');
 
+    if(signUpErrorAlert) {
+    signUpErrorAlert.classList.add('hidden');
+    }
+
+    if (logInForm.classList.contains('hidden')) {
+      formHolder.classList.remove('hidden');
+      logInForm.classList.remove('hidden');
+      demoUserForm.classList.add('hidden');
+      signUpForm.classList.add('hidden');
     } else {
-        signUpForm.classList.remove('hidden');
-        formHolder.classList.remove('hidden')
-        signUpButton.classList.add('hidden')
-}
+      logInForm.classList.add('hidden');
+    }
+  });
 
-    logInButton.addEventListener('click', (event) => {
-        logInButton.classList.add('hidden');
-        signUpButton.classList.remove('hidden')
-        demoUserButton.classList.remove('hidden')
+  signUpButton.addEventListener('click', (event) => {
+    signUpButton.classList.add('hidden');
+    logInButton.classList.remove('hidden');
+    demoUserButton.classList.remove('hidden');
 
-        if(logInForm.classList.contains('hidden')) {
-            formHolder.classList.remove('hidden')
-            logInForm.classList.remove('hidden');
-            demoUserForm.classList.add('hidden');
-            signUpForm.classList.add('hidden');
-        } else {
-            logInForm.classList.add('hidden');
+    if(loginErrorAlert) {
+    loginErrorAlert.classList.add('hidden');
+    }
 
-        }
-    })
+    if (signUpForm.classList.contains('hidden')) {
+      formHolder.classList.remove('hidden');
+      signUpForm.classList.remove('hidden');
+      logInForm.classList.add('hidden');
+      demoUserForm.classList.add('hidden');
+    } else {
+      signUpForm.classList.add('hidden');
+    }
+  });
 
-    signUpButton.addEventListener('click', (event) => {
-        signUpButton.classList.add('hidden');
-        logInButton.classList.remove('hidden')
-        demoUserButton.classList.remove('hidden')
-        if(signUpForm.classList.contains('hidden')) {
-            formHolder.classList.remove('hidden')
-            signUpForm.classList.remove('hidden');
-            logInForm.classList.add('hidden');
-            demoUserForm.classList.add('hidden');
-        } else {
-            signUpForm.classList.add('hidden');
+  demoUserButton.addEventListener('click', (event) => {
+    logInButton.classList.remove('hidden');
+    signUpButton.classList.remove('hidden');
+    demoUserButton.classList.add('hidden');
 
-        }
-    })
+   if(loginErrorAlert) {
+    loginErrorAlert.classList.add('hidden');
+   }
 
-    demoUserButton.addEventListener('click', (event) => {
-
-        logInButton.classList.remove('hidden');
-        signUpButton.classList.remove('hidden')
-        demoUserButton.classList.add('hidden')
-
-        if(demoUserForm.classList.contains('hidden')) {
-            formHolder.classList.remove('hidden')
-            demoUserForm.classList.remove('hidden');
-            logInForm.classList.add('hidden');
-            signUpForm.classList.add('hidden');
-        } else {
-            demoUserForm.classList.add('hidden');
-
-        }
-    })
+   if(signUpErrorAlert) {
+    signUpErrorAlert.classList.add('hidden');
+   }
+    if (demoUserForm.classList.contains('hidden')) {
+      formHolder.classList.remove('hidden');
+      demoUserForm.classList.remove('hidden');
+      logInForm.classList.add('hidden');
+      signUpForm.classList.add('hidden');
+    } else {
+      demoUserForm.classList.add('hidden');
+    }
+  });
 
 
-})
+
+
+});
